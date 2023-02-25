b7833d016856ed1d7905a483672a692f811c4667
caiozirretta <caiozirretta@hotmail.com>
2023-02-25 13:04:41 -0300

    basics

---
+++ src/domain/sheet/Sheet.ts
@@ -0,0 +1,27 @@
+export type protection = {
+
+}
+
+export type resistance = {
+
+}
+
+export type skill = {
+
+}
+
+export type attacks = {
+
+}
+
+export type abilities = {
+
+}
+
+export type inventory = {
+
+}
+
+export type description = {
+
+}
